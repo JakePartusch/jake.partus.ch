306d79c99d5a53a73e03a3c8e12a36aa64f4c352
Jake Partusch <jakepartusch@gmail.com>
2019-05-15 22:57:34 -0500

    feat: update website content. Add sorting (#10)

--- src/components/Home/HeroHeader.js
+++ src/components/Home/HeroHeader.js
@@ -1,67 +1,65 @@
-import React, { Component } from 'react';
-import styled from '@emotion/styled';
+import React, { Component } from "react";
+import styled from "@emotion/styled";
 
 const HeroSegment = styled.div({
-    minHeight: '700px', 
-    padding: '1em 0em',
-    background: '#1B1C1D',
-    color: 'rgba(255, 255, 255, .9)',
-    textAlign: 'center',
-    margin: '0',
-    paddingLeft: '0',
-    paddingRight: '0',
-    borderBottom: '1px solid rgba(34, 36, 38, .15)'
+  minHeight: "700px",
+  padding: "1em 0em",
+  background: "#1B1C1D",
+  color: "rgba(255, 255, 255, .9)",
+  textAlign: "center",
+  margin: "0",
+  paddingLeft: "0",
+  paddingRight: "0",
+  borderBottom: "1px solid rgba(34, 36, 38, .15)"
 });
 
 const HeroHeading = styled.div({
-    marginBottom: '0',
-    marginTop: '8em' 
+  marginBottom: "0",
+  marginTop: "8em"
 });
 
 const NameHeader = styled.h1({
-    fontSize: '4em'
+  fontSize: "4em"
 });
 
 const HeroSubHeading = styled.h4({
-    fontSize: '1.3em',
-    marginBottom: '0.25em'
-})
+  fontSize: "1.3em",
+  marginBottom: "0.25em"
+});
 
 const Container = styled.div({
-    maxWidth: '700px',
-    lineHeight: '1.5',
-    margin: 'auto'
+  maxWidth: "700px",
+  lineHeight: "1.5",
+  margin: "auto"
 });
 
 const Avatar = styled.img({
-    width: '150px',
-    height: 'auto',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    borderRadius: '500em',
-    overflow: 'hidden',
-    border: '#666 2px solid',
-    boxShadow: '0px 0px 3px 0px #666'
-
+  width: "150px",
+  height: "auto",
+  marginLeft: "auto",
+  marginRight: "auto",
+  borderRadius: "500em",
+  overflow: "hidden",
+  border: "#666 2px solid",
+  boxShadow: "0px 0px 3px 0px #666"
 });
 
 export default class HeroHeader extends Component {
-    
-    render() {
-        return (
-            <HeroSegment>
-                <Container>
-                    <HeroHeading>
-                        <Avatar src={this.props.avatar}/>
-                        <NameHeader>Jake Partusch</NameHeader>
-                    </HeroHeading>
-                    <HeroSubHeading>Dad. 👪</HeroSubHeading>
-                    <HeroSubHeading>Full-Stack Developer. 💻</HeroSubHeading>
-                    <HeroSubHeading>Golden retriever lover. 🐶</HeroSubHeading>
-                    <HeroSubHeading>Traveler. ✈️</HeroSubHeading>
-                    <HeroSubHeading>Homebrewer. 🍻</HeroSubHeading>
-                </Container>
-            </HeroSegment>
-        )
-    }
+  render() {
+    return (
+      <HeroSegment>
+        <Container>
+          <HeroHeading>
+            <Avatar src={this.props.avatar} />
+            <NameHeader>Jake Partusch</NameHeader>
+          </HeroHeading>
+          <HeroSubHeading>Dad. 👪</HeroSubHeading>
+          <HeroSubHeading>JavaScript Developer. 💻</HeroSubHeading>
+          <HeroSubHeading>Golden retriever lover. 🐶</HeroSubHeading>
+          <HeroSubHeading>Traveler. ✈️</HeroSubHeading>
+          <HeroSubHeading>Homebrewer. 🍻</HeroSubHeading>
+        </Container>
+      </HeroSegment>
+    );
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,73 +1,89 @@
-import React from 'react'
-import Home from '../components/Home'
+import React from "react";
+import Home from "../components/Home";
 
 export default class Index extends React.Component {
   render() {
-    const frontendLogos = this.props.data.frontendLogos.edges.map(image => image.node.resize.src);
-    const backendLogos = this.props.data.backendLogos.edges.map(image => image.node.resize.src);
-    const avatar = this.props.data.avatar.edges.map(image => image.node.resize.src)[0];
-    const projects = this.props.data.projects.edges.map(project => ({ ...project.node.frontmatter }))
+    const frontendLogos = this.props.data.frontendLogos.edges.map(
+      image => image.node.resize.src
+    );
+    const backendLogos = this.props.data.backendLogos.edges.map(
+      image => image.node.resize.src
+    );
+    const avatar = this.props.data.avatar.edges.map(
+      image => image.node.resize.src
+    )[0];
+    const projects = this.props.data.projects.edges.map(project => ({
+      ...project.node.frontmatter
+    }));
     return (
       <div>
         <Home
-          avatar={avatar} 
+          avatar={avatar}
           frontendLogos={frontendLogos}
           backendLogos={backendLogos}
           projects={projects}
         />
       </div>
-    )
+    );
   }
 }
 export const pageQuery = graphql`
-query IndexQuery {
-  projects: allMarkdownRemark(
-    filter: { id: { regex: "/work/" } }
-  ) {
-    edges {
-      node {
-        ... on MarkdownRemark {
-          frontmatter {
-            title
-            content
-            siteLink
-            sourceLink
+  query IndexQuery {
+    projects: allMarkdownRemark(
+      filter: { id: { regex: "/work/" } }
+      sort: { fields: [fileAbsolutePath], order: ASC }
+    ) {
+      edges {
+        node {
+          ... on MarkdownRemark {
+            frontmatter {
+              title
+              content
+              siteLink
+              sourceLink
+            }
+          }
+        }
+      }
+    }
+    avatar: allImageSharp(filter: { id: { regex: "/avatar.jpg/" } }) {
+      edges {
+        node {
+          ... on ImageSharp {
+            resize(width: 300, height: 300) {
+              src
+            }
+          }
+        }
+      }
+    }
+    backendLogos: allImageSharp(
+      filter: { id: { regex: "/backend/" } }
+      sort: { fields: [original___src], order: ASC }
+    ) {
+      edges {
+        node {
+          ... on ImageSharp {
+            resize(width: 200, height: 200) {
+              src
+            }
+          }
+        }
+      }
+    }
+    frontendLogos: allImageSharp(
+      filter: { id: { regex: "/frontend/" } }
+      sort: { fields: [original___src], order: ASC }
+    ) {
+      edges {
+        node {
+          ... on ImageSharp {
+            resize(width: 200, height: 200) {
+              src
+            }
           }
         }
       }
     }
   }
-  avatar: allImageSharp(filter: { id: { regex: "/avatar.jpg/" } }) { 
-    edges {
-      node {
-        ... on ImageSharp {
-          resize(width: 300, height: 300) {
-            src
-          }
-        }
-      }
-    }
-  }
-  backendLogos: allImageSharp(filter: { id: { regex: "/backend/" } }) { 
-    edges {
-      node {
-        ... on ImageSharp {
-          resize(width: 200, height: 200) {
-            src
-          }
-        }
-      }
-    }
-  }
-  frontendLogos: allImageSharp(filter: { id: { regex: "/frontend/" } }) { 
-    edges {
-      node {
-        ... on ImageSharp {
-          resize(width: 200, height: 200) {
-            src
-          }
-        }
-      }
-    }
-  }
-}`
+`;
