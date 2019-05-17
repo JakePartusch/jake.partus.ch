6659e330f35e4f0e4de69993de3084f81c0b9928
Jake Partusch <jakepartusch@gmail.com>
2019-05-16 22:05:26 -0500

    feat: adding hero image (#11)

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
-import { SocialIcon } from 'react-social-icons';
-import styled from '@emotion/styled'
-
+import React, { Component } from "react";
+import { SocialIcon } from "react-social-icons";
+import styled from "@emotion/styled";
 
 const FooterContainer = styled.div({
-    backgroundColor: '#1B1C1D'
+  backgroundColor: "#1B1C1D"
 });
 
-const FooterMessage = styled.div({
-    paddingTop: '1em',
-    marginLeft: '0.25em',
-    color: '#FFF',
-    textAlign: 'center'
-})
-
 const StyledSocialIcon = styled(SocialIcon)({
-    margin: '0.25em'
-})
+  margin: "0.25em"
+});
 
 const SocialIcons = styled.div({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingBottom: '1em'
-})
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "0.5em"
+});
 
 export default class Footer extends Component {
-
-    render() {
-        return (
-            <FooterContainer>
-                <FooterMessage>Made by yours truly 😊</FooterMessage>
-                <SocialIcons>
-                    <StyledSocialIcon url="https://twitter.com/JakePartusch"/>
-                    <StyledSocialIcon url="https://github.com/JakePartusch" />
-                    <StyledSocialIcon url="https://www.linkedin.com/in/jacob-partusch-4a181b79/"/>
-                </SocialIcons>
-            </FooterContainer>
-        )
-    }
+  render() {
+    return (
+      <FooterContainer>
+        <SocialIcons>
+          <StyledSocialIcon url="https://twitter.com/JakePartusch" />
+          <StyledSocialIcon url="https://github.com/JakePartusch" />
+          <StyledSocialIcon url="https://www.linkedin.com/in/jacob-partusch-4a181b79/" />
+        </SocialIcons>
+      </FooterContainer>
+    );
+  }
 }
