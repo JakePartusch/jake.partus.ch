222bc7ce9fa828ae9fd89086d9e4826cf8a1b63c
Jake Partusch <jakepartusch@gmail.com>
2019-05-24 22:51:25 -0500

    feat: adding bio section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import HeroHeader from "./HeroHeader";
+import Bio from "./Bio";
 import Skills from "./Skills";
 import Footer from "./Footer";
+
 export default class Home extends Component {
   render() {
     const { avatar } = this.props;
     return (
       <div>
         <HeroHeader avatar={avatar} />
+        <Bio />
         <Skills />
         <Footer />
       </div>
