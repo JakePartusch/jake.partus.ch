55e4b9740ff489454672457cfd2a20d261ecab57
Jake Partusch <jakepartusch@gmail.com>
2019-09-18 13:57:57 -0500

    feat: improving the script logs

--- scripts/performanceEvaluation.js
+++ scripts/performanceEvaluation.js
@@ -2,7 +2,7 @@
 const exec = require("child_process").exec;
 
 const main = async () => {
-  console.log("Deploying to Netlify...");
+  console.log("netlify deploy --json");
   const deployPromise = new Promise((resolve, reject) => {
     exec(`netlify deploy --json`, (error, stdout, stderr) => {
       if (error) {
@@ -16,15 +16,18 @@ const main = async () => {
 
   console.log("Deploy URL:", deploy_url);
 
-  console.log("Running Lighthouse evaluation...");
+  console.log(`npx @laas/lighthouse-ci ${deploy_url} --threshold=90`);
 
   const performancePromise = new Promise((resolve, reject) => {
-    exec(`npx @laas/lighthouse-ci ${deploy_url}`, (error, stdout, stderr) => {
-      if (error) {
-        console.warn(error);
+    exec(
+      `npx @laas/lighthouse-ci ${deploy_url} --threshold=90`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.warn(error);
+        }
+        resolve(stdout ? stdout : stderr);
       }
-      resolve(stdout ? stdout : stderr);
-    });
+    );
   });
   console.log(await performancePromise);
 };
