0218db8a4cf862032fe6ad0ce957e199c0d6fdc1
Jacob Partusch <jakepartusch@gmail.com>
2020-01-04 20:27:58 -0600

    feat: attempting manual exec in netlify plugin

--- plugins/plugin-lighthouse/index.js
+++ plugins/plugin-lighthouse/index.js
@@ -1,14 +1,32 @@
 const execa = require("execa");
+const exec = require("child_process").exec;
+
+const promisifyExec = cmd => {
+  console.log(cmd);
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.warn(error);
+        reject(error);
+      }
+      resolve(stdout ? stdout : stderr);
+    });
+  });
+};
 
 module.exports = {
   name: "netlify-plugin-lighthouse",
   onPostBuild: async () => {
     try {
-      const { stdout } = await execa("lhci", [
-        "autorun",
-        "--upload.target=temporary-public-storage"
-      ]);
-      console.log(stdout);
+      // const { stdout } = await execa("lhci", [
+      //   "autorun",
+      //   "--upload.target=temporary-public-storage"
+      // ]);
+      // console.log(stdout);
+      const result = await promisifyExec(
+        "lhci autorun --upload.target=temporary-public-storage"
+      );
+      console.log(result);
     } catch (e) {
       console.log(e);
     }
