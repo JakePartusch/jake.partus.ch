3e4dcbc3f2e8ff135cbf0dc112d66878a561cd07
Jacob Partusch <jakepartusch@gmail.com>
2020-01-04 20:17:34 -0600

    feat: log exec output

--- plugins/plugin-lighthouse/index.js
+++ plugins/plugin-lighthouse/index.js
@@ -3,10 +3,14 @@ const execa = require("execa");
 module.exports = {
   name: "netlify-plugin-lighthouse",
   onPostBuild: async () => {
-    const { stdout } = await execa("lhci", [
-      "autorun",
-      "--upload.target=temporary-public-storage"
-    ]);
-    console.log(stdout);
+    try {
+      const { stdout } = await execa("lhci", [
+        "autorun",
+        "--upload.target=temporary-public-storage"
+      ]);
+      console.log(stdout);
+    } catch (e) {
+      console.log(e);
+    }
   }
 };
