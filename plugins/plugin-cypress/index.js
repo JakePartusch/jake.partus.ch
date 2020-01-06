81152fe160f397d35567b0b46cfae21f2303f5f8
Jacob Partusch <jakepartusch@gmail.com>
2020-01-05 21:01:18 -0600

    fix: cleaning up netlify plugins

--- plugins/plugin-cypress/index.js
+++ plugins/plugin-cypress/index.js
@@ -1,32 +1,15 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
-
 module.exports = {
   name: "netlify-plugin-cypress",
   onEnd: async () => {
-    console.log(process.env);
     try {
-      // const { stdout, stderr } = await exec(
-      //   `cypress run --config baseUrl='${process.env.DEPLOY_URL}'`
-      // );
-      // require("child_process").spawnSync(
-      //   "cypress",
-      //   ["run", "--config", `baseUrl=${process.env.DEPLOY_URL}`],
-      //   {
-      //     cwd: process.cwd(),
-      //     stdio: "inherit"
-      //   }
-      // );
       require("child_process").spawnSync(
-        "curl",
-        [process.env.DEPLOY_PRIME_URL],
+        "cypress",
+        ["run", "--config", `baseUrl=${process.env.DEPLOY_URL}`],
         {
           cwd: process.cwd(),
           stdio: "inherit"
         }
       );
-      // console.log(stdout);
-      // console.log(stderr);
     } catch (e) {
       console.log(e);
     }
--- plugins/plugin-lighthouse/index.js
+++ plugins/plugin-lighthouse/index.js
@@ -1,25 +1,13 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
-
 module.exports = {
   name: "netlify-plugin-lighthouse",
-  onEnd: async () => {
-    console.log(process.env);
-    try {
-      // const { stdout } = await execa("lhci", [
-      //   "autorun",
-      //   "--upload.target=temporary-public-storage"
-      // ]);
-      // const { stdout, stderr } = await exec(
-      //   `npx @lhci/cli autorun --upload.target=temporary-public-storage`
-      // );
-      const { stdout, stderr } = await exec(
-        `npx @laas/lighthouse-ci ${process.env.DEPLOY_URL}`
-      );
-      console.log(stdout);
-      console.log(stderr);
-    } catch (e) {
-      console.log(e);
-    }
+  onEnd: () => {
+    require("child_process").spawnSync(
+      "npx",
+      ["@lhci/cli", "autorun", "--upload.target=temporary-public-storage"],
+      {
+        cwd: process.cwd(),
+        stdio: "inherit"
+      }
+    );
   }
 };
