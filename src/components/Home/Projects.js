0e6b35c4687ccdbfc872ab09309324579b212f5c
Partusch <req92678@c02yq09rlvcf.corp.mutualofomaha.com>
2020-01-13 13:44:52 -0600

    feat: adding gh stars to projects

--- src/components/Home/Projects.js
+++ src/components/Home/Projects.js
@@ -3,7 +3,10 @@ import styled from "@emotion/styled";
 import { useStaticQuery, graphql } from "gatsby";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faExternalLinkSquareAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faExternalLinkSquareAlt,
+  faStar
+} from "@fortawesome/free-solid-svg-icons";
 import useMedia from "use-media";
 
 const ProjectList = styled.ul(({ isWide }) => {
@@ -52,43 +55,47 @@ const Projects = () => {
         return (
           <li
             key={project.title}
-            className="flex flex-col bg-white rounded-lg p-8 justify-between shadow-lg"
+            className="relative flex flex-col bg-white rounded-lg justify-between shadow-lg pb-8"
           >
-            <div className="flex flex-col justify-center items-center text-center mb-2">
-              <h2 className="text-gray-700 text-xl font-bold mb-4">
+            {project.stars && (
+              <div className="absolute top-0 text-sm text-red-900 right-0 rounded-tl-lg p-2">
+                <span className="mr-2">{project.stars}</span>
+                <FontAwesomeIcon icon={faStar} />
+              </div>
+            )}
+            <div className="flex flex-col justify-center items-center text-center mb-2 mt-8">
+              <h2 className="text-gray-700 text-xl font-bold mb-4 mx-10">
                 {project.title}
               </h2>
-              <p className="text-gray-900">{project.content}</p>
+              <p className="text-gray-900 mx-6">{project.content}</p>
             </div>
-            <div>
-              <div className="mt-8 flex items-center justify-center">
-                <a
-                  href={project.sourceLink}
-                  target="_"
-                  rel="noopener"
-                  className="rounded px-3 py-2 bg-white hover:bg-gray-200 text-gray-800 font-semibold leading-tight shadow-md mr-2"
-                >
-                  Source
-                  <FontAwesomeIcon
-                    icon={faGithub}
-                    size="lg"
-                    className="ml-2 text-gray-900"
-                  />
-                </a>
-                <a
-                  href={project.siteLink}
-                  target="_"
-                  rel="noopener"
-                  className="rounded px-3 py-2 bg-blue-600 hover:bg-blue-600 text-gray-100 font-semibold leading-tight shadow-md"
-                >
-                  Website
-                  <FontAwesomeIcon
-                    icon={faExternalLinkSquareAlt}
-                    size="lg"
-                    className="ml-2 text-gray-100"
-                  />
-                </a>
-              </div>
+            <div className="mt-8 mb-8 flex items-center justify-center">
+              <a
+                href={project.sourceLink}
+                target="_"
+                rel="noopener"
+                className="rounded px-3 py-2 bg-white hover:bg-gray-200 text-gray-800 font-semibold leading-tight shadow-md mr-2"
+              >
+                Source
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  size="lg"
+                  className="ml-2 text-gray-900"
+                />
+              </a>
+              <a
+                href={project.siteLink}
+                target="_"
+                rel="noopener"
+                className="rounded px-3 py-2 bg-blue-600 hover:bg-blue-600 text-gray-100 font-semibold leading-tight shadow-md"
+              >
+                Website
+                <FontAwesomeIcon
+                  icon={faExternalLinkSquareAlt}
+                  size="lg"
+                  className="ml-2 text-gray-100"
+                />
+              </a>
             </div>
           </li>
         );
